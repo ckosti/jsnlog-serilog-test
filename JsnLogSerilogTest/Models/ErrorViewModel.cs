96a22afb1716d30780d76c1cc0127ad0c8ae5bd4
Collin Kostichuk <collin.kostichuk@genivar.com>
2019-06-12 09:54:52 -0600

    Add project files.

---
+++ JsnLogSerilogTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JsnLogSerilogTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
